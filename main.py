c55296162609f6c805bf556cd4752d83aed6d676
Zick Keen <jackkinny@gmail.com>
2022-08-09 21:56:53 +0700

    perbaikan update

--- main.py
+++ main.py
@@ -8,19 +8,14 @@ populasi = st.number_input("Masukan Populasi",format="%.0f")
 galatpendugaan = 0.05
 proporsi = 0.5
 sampel = st.button("Hitung Sampel")
-interval = st.button ("Lihat Interval")
 
 if (bool(populasi)):
     results = (chikuadrat * populasi * proporsi * (1-proporsi)) / ((populasi - 1) * (galatpendugaan**2) + chikuadrat * proporsi *(1-proporsi))
-    st.write("Jumlah Sampel Verifikasi Faktualnya adalah", round(results))
+    st.write("Jumlah Sampel Verifikasi Faktual: ", round(results))
+    interval = populasi / results
+    st.write("dengan interval: ", round(interval))
     
 elif(populasi == 0):
   st.write("Populasi Masih kosong")
 else:
     st.write("Data gagal diproses")
-
-# Interval = st.write('hasil')
-
-if interval: 
-    interval = populasi / results
-    st.write("Intervalnya adalah", round(interval,2))
